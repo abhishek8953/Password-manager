c17cd98e580e044a73bfc8f9e16bf16ba2d39e63
abhishek8953 <rajantiwariat@gmail.com>
2024-05-01 14:08:23 +0530

    new

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -43,6 +43,7 @@ async function login(req, res) {
         res.cookie("Authorization", token, {
             expires: new Date(exp),
             httpOnly: true,
+            secure:true,
             sameSite: "lax"  //meaning 
             
         });
