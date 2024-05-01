3c97111c94ef65d6dc29eafa89388a362fded5fa
Abhishek Tiwari <51175997+abhishek8953@users.noreply.github.com>
2024-05-01 14:44:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        
-        origin: process.env.ORIGIN,
+        origin: "*",
+        // origin: process.env.ORIGIN,
         credentials: true,
     })
 );
