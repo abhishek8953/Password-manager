866f0e102a92a111302787b788de268c4e091b36
Abhishek Tiwari <51175997+abhishek8953@users.noreply.github.com>
2024-05-01 15:19:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,21 +20,16 @@ connectToDb();
 //congigure to use json
 app.use(express.json());
 app.use(cookieParser());
-// app.use(
-//     cors({
-//         origin: "*",
-//         // origin: process.env.ORIGIN,
-//         credentials: true,
-//     })
-// );
+app.use(
+    cors({
+      
+        origin: process.env.ORIGIN,
+        credentials: true,
+    })
+);
 
-// app.use(cors());
 
-app.use(cors({
-    origin: process.env.ORIGIN,
-    headers: ["Content-Type"],
-    credentials: true,
-}));
+
 
 
 //destructuring all functions from the notes controller
